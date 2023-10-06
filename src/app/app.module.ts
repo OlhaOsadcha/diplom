9167c66187fbb885c87a2e86e5c32025656b71ea
SergiyOsadchyy <sergiy.osadchyy@gmail.com>
2023-10-06 17:09:31 +0100

    feat: Add Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {MatButtonModule} from "@angular/material/button";
+import {NgModule} from "@angular/core";
+import {AppComponent} from "./app.component";
+import {StartPageComponent} from "./start-page/start-page.component";
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {CommonModule} from "@angular/common";
 
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartPageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
